f5f282bb3a5670448e248e267d6b8550da318ee9
Samuel Backlund <me@backer8002.com>
2026-03-22 10:19:14 +0100

    =[Regex] Continued working. Currently missing correct parsing of unions.

--- Parsing/Regex.c
+++ Parsing/Regex.c
@@ -48,55 +48,78 @@ static BL_DynamicContainer internal_make_regex_ast(BL_UnicodeView expr) {
         bl_log_debug_location("%s","The empty word is not supported.");
         return (BL_DynamicContainer){0};
     }
-    BL_DynamicContainer  result = bl_container_dynamic_create_stack(bl_unicodestr_length(expr), sizeof(struct RegexASTNode*));
+    BL_DynamicContainer  result = bl_container_dynamic_create_stack(bl_unicodestr_length(expr), sizeof(struct RegexASTNode));
     if (!bl_container_dynamic_is_valid(&result))
         return result;
     BL_DynamicContainer  stack  = bl_container_dynamic_create_stack(0, sizeof(struct RegexASTNode));
-
-    bl_container_dynamic_append(&result, sizeof(struct RegexASTNode), &(struct RegexASTNode){.operation = RegexOpConcat,.firstNode = NULL});
-    struct RegexASTNode* current   = bl_container_dynamic_back(&result);
+    struct RegexASTNode* prev = NULL;
+    struct RegexASTNode current = {.operation = RegexOpConcat,.firstNode = NULL};
+    size_t amountOfStopablePoints = 0;
 
     for (const BL_Unicodepoint* character = bl_unicodestr_front(expr); character; character = bl_unicodestr_next(expr, character)) {
         if (*character == U'(') {
-            BL_ContainerError errorCode = bl_container_dynamic_append(&result, sizeof(struct RegexASTNode*), &current);
+            BL_ContainerError errorCode = bl_container_dynamic_append(&result, sizeof current, &current);
             if (errorCode != BL_ContainerOPSuccessful) {
                 bl_log_debug_location("%s", "Unable to push to stack");
-				goto Cleanup;
+	      			  goto Cleanup;
             }
-            if (bl_container_dynamic_append(&result, sizeof(struct RegexASTNode), &(struct RegexASTNode){.firstNode = NULL,.operation = RegexOpConcat}) != BL_ContainerOPSuccessful)
-                goto Cleanup;
-            current = bl_container_dynamic_back(&result);
+            current = (struct RegexASTNode){.operation = RegexOpConcat, .firstNode = NULL};
             continue;
         }
         if (*character == U')') {
             if (bl_container_dynamic_is_empty(&stack)) {
                 bl_log_debug_location("%s", "Unexpected ')' which closes nonexistant scope.");
-				goto Cleanup;
+				        goto Cleanup;
             }
-			struct RegexASTNode* currentNode = *(struct RegexASTNode**)bl_container_dynamic_back(&stack);
+			      current.secondOp = (struct RegexASTOp){.isAtom = false,.node = (struct RegexASTNode*)bl_container_dynamic_back(&stack)};
             bl_container_dynamic_pop(&stack);
-            currentNode->secondOp = (struct RegexASTOp ) {.isAtom = false,.node = current};
-            if (bl_container_dynamic_append(&result, sizeof *currentNode, currentNode) != BL_ContainerOPSuccessful)
-                goto Cleanup;
-            current = bl_container_dynamic_back(&result);
-            continue;
-        }
-        if (*character == U'|') {
-            if (current->operation != RegexOpUnion) {
-                current->operation = RegexOpUnion;
+
+        } else if (*character == U'|') {
+            if (current.operation != RegexOpUnion && current.firstNode) {
+                current.operation = RegexOpUnion;
                 continue;
             }
+            current.secondOp = (struct RegexASTOp){.isAtom = true, .codepoint = *character};
         } else if (*character == U'[') {
             #error Do character range stuff;
-        } else if (*character == U'*') {
-            if (!current->firstNode) {
-                current->secondOp = (struct RegexASTOp){.isAtom = true,.codepoint = *character};
-            } else {
-                current->operation = RegexOpRepeatGreedy;
-            }
-        } else if (*character == U'?') {
-            if (current->firstNode && current->firstNode->operation == ) // Confusing system. fix
+        } else
+            current.secondOp = (struct RegexASTOp){.isAtom = true, .codepoint = *character};
+        
+        if (current.operation == RegexOpUnion) {
+            
         }
+
+        BL_Unicodepoint* optionalModifier1 = bl_unicodestr_next(expr,character);
+        if (optionalModifier1 && (*optionalModifier1 == U'?' || *optionalModifier1 == U'+' || *optionalModifier1 == U'*')) {
+            character = optionalModifier1;
+            amountOfStopablePoints++;
+
+            if (*optionalModifier1 == U'?') {
+                struct RegexASTNode optional = {.operation = RegexOpOptional,.secondOp = current.secondOp};
+                if (bl_container_dynamic_append(&result,sizeof optional,&optional) != BL_ContainerOPSuccessful)
+                    goto Cleanup;
+
+                current.secondOp = (struct RegexASTOp){.isAtom = false, .node = bl_container_dynamic_back(&result)};
+            } else {
+                if (*optionalModifier1 == '+') {
+                    if (bl_container_dynamic_append(&result, sizeof current, &current) != BL_ContainerOPSuccessful)
+                        goto Cleanup;
+                    current.firstNode = bl_container_dynamic_back(&result);
+                }
+
+                BL_Unicodepoint* optionalModifier2 = bl_unicodestr_next(expr,character);
+                if (optionalModifier2 && *optionalModifier2 == U'?')
+                    character = optionalModifier2;
+
+                struct RegexASTNode repeatingNode = {.operation = *character == U'?' ? RegexOpRepeat : RegexOpRepeatGreedy, .secondOp = current.secondOp};
+                if (bl_container_dynamic_append(&result, sizeof repeatingNode, &repeatingNode) != BL_ContainerOPSuccessful)
+                    goto Cleanup;
+                current.secondOp = (struct RegexASTOp){.isAtom = false, .node = bl_container_dynamic_back(&result)};
+            }
+        }
+        if (bl_container_dynamic_append(&result,sizeof current, &current) != BL_ContainerOPSuccessful)
+            goto Cleanup;
+        current = (struct RegexASTNode){.operation = RegexOpConcat,.firstNode = bl_container_dynamic_back(&result)};
     }
 
     return result;
